9b46a1322d9eb7e96e62c7f5479aa42ea9a4f588
Giuseppe <ggangix@gmail.com>
2021-12-26 16:32:09 +0100

    First implemetation countries view

--- src/views/Countries/index.jsx
+++ src/views/Countries/index.jsx
@@ -1,8 +1,97 @@
-import React from "react";
+import React, { useEffect, useState, useMemo, useCallback } from "react";
+import Api from "../../services/api";
+import { useNavigate } from "react-router-dom";
 import "./style.scss";
 
 function Countries() {
-  return <div className="countries"></div>;
+  const [countries, setCountries] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getAllCountries();
+  }, []);
+
+  const filteredCountries = useMemo(() => {
+    console.log(countries);
+    if (countries.length === 0) return [];
+    return countries.filter((country) => {
+      return country.name.toLowerCase().includes(search.toLowerCase());
+    });
+  }, [countries, search]);
+
+  const searchCountries = useCallback((e) => {
+    setSearch(e.target.value);
+  }, []);
+
+  const getAllCountries = async (username) => {
+    try {
+      const countries = await Api.getCountries();
+      setCountries(countries);
+    } catch (err) {
+      localStorage.removeItem("token");
+      window.location.reload();
+    }
+  };
+
+  const handleDelete = async (countryCode) => {
+    try {
+      await Api.deleteCountry(countryCode);
+      getAllCountries();
+    } catch (err) {
+      localStorage.removeItem("token");
+      window.location.reload();
+    }
+  };
+
+  const handleEdit = (countryCode) => {
+    navigate(`/countries/${countryCode}`);
+  };
+
+  return (
+    <div className="countries">
+      <input
+        type="text"
+        onChange={searchCountries}
+        placeholder="Search for a country"
+        disabled={!countries}
+      />
+      {filteredCountries.length === 0 && (
+        <p className="noCountries">No countries found</p>
+      )}
+      <div className="countriesData">
+        {filteredCountries.map((country) => {
+          return (
+            <div className="countryItem" key={country.code}>
+              <div>
+                <h3>{country.code}</h3>
+              </div>
+              <div>
+                <p>{country.name}</p>
+              </div>
+              <div className="controls">
+                <button
+                  className="edit"
+                  onClick={() => handleEdit(country.code)}
+                >
+                  Edit
+                </button>
+
+                <button
+                  className="delete"
+                  onClick={() => handleDelete(country.code)}
+                >
+                  {" "}
+                  Delete
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Countries;
