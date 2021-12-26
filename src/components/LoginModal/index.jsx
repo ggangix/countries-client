58654f3147576c9e08125c432fb9eb9a4cb0f82a
Giuseppe <ggangix@gmail.com>
2021-12-26 14:59:16 +0100

    Add form handlers to submit login data

--- src/components/LoginModal/index.jsx
+++ src/components/LoginModal/index.jsx
@@ -1,20 +1,43 @@
 import React from "react";
 import "./style.scss";
+import Api from "../../services/api";
 
 function LoginModal() {
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [error, setError] = React.useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    Api.login(username, password)
+      .then((data) => {
+        console.log(data);
+        if (data.message !== "User logged in") {
+          return setError(data.message);
+        }
+        if (data.token) localStorage.setItem("token", data.token);
+        window.location.reload();
+      })
+      .catch((err) => {
+        setError(err.message);
+      });
+  };
+
   return (
     <div id="login-modal" className="modal">
       <div className="modal-content">
         <div className="modal-header">
           <h4 className="modal-title">Login</h4>
         </div>
+        {error && <div className="error">{error}</div>}
         <div className="modal-body">
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="form-group">
               <label htmlFor="username">Username</label>
               <input
                 type="text"
                 className="form-control"
+                onChange={(e) => setUsername(e.target.value)}
                 id="username"
                 placeholder="Enter username"
               />
@@ -24,11 +47,14 @@ function LoginModal() {
               <input
                 type="password"
                 className="form-control"
+                onChange={(e) => setPassword(e.target.value)}
                 id="password"
                 placeholder="Enter password"
               />
             </div>
-            <button type="submit">Login</button>
+            <button type="submit" onSubmit={handleSubmit}>
+              Login
+            </button>
           </form>
         </div>
       </div>
